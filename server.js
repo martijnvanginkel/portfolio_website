c4bfcdaddf10ad4a467566308e5820f85c58c4ed
Martijn van Ginkel <martijnvanginkel2@gmail.com>
2020-04-07 21:05:32 +0200

    Added server env and added page transitions

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', async (req, res) => {
+    res.render('about');
+});
+
+app.listen(5000);
