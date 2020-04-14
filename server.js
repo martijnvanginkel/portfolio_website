c073c8f5417ba9ea75356a1a83b60140d52c022f
Martijn van Ginkel <martijnvanginkel2@gmail.com>
2020-04-14 13:35:48 +0200

    Added links to project pages

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const app = express();
 
 app.set('view engine', 'ejs');
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
@@ -16,6 +15,26 @@ app.get('/work', async (req, res) => {
 
 app.get('/weurd', async (req, res) => {
     res.render('weurd');
+});
+
+app.get('/the_journey', async (req, res) => {
+    res.render('the_journey');
+});
+
+app.get('/cms', async (req, res) => {
+    res.render('cms');
+});
+
+app.get('/get_next_line', async (req, res) => {
+    res.render('get_next_line');
+});
+
+app.get('/pixel_farm', async (req, res) => {
+    res.render('pixel_farm');
+});
+
+app.get('/raycasting', async (req, res) => {
+    res.render('raycasting');
 });
 
 app.listen(5000);
